14499e992fecbc5bb517ea033e59fd9eac35c437
Alexander Ovchinikov <alexo2you@gmail.com>
2019-02-18 11:14:17 +0200

    Update audio_drv.h

--- software driver example/audio_drv.h
+++ software driver example/audio_drv.h
@@ -10,35 +10,35 @@
 
 #include "sys/alt_dev.h"
 // -------------------- TLV320AIC1106 definitions
-#define AIC1106_p3db			0
-#define AIC1106_0db				1
-#define AIC1106_m3db			2
-#define AIC1106_m6db			3
-#define AIC1106_m9db			4
-#define AIC1106_m12db			5
-#define AIC1106_m15db			6
-#define AIC1106_m18db			7
-#define AIC1106_off				AIC1106_MUTE
+#define AIC1106_p3db			    0 
+#define AIC1106_0db				    1
+#define AIC1106_m3db			    2
+#define AIC1106_m6db			    3
+#define AIC1106_m9db			    4
+#define AIC1106_m12db			    5
+#define AIC1106_m15db			    6
+#define AIC1106_m18db			    7
+#define AIC1106_off				    AIC1106_MUTE
 
-#define AIC1106_MUTE			0x08
-#define AIC1106_ENABLE			0x10
-#define AIC1106_LOOPBACK		0x20
-#define AIC1106_RESET			0x40
+#define AIC1106_MUTE			    0x08
+#define AIC1106_ENABLE		    0x10
+#define AIC1106_LOOPBACK	    0x20
+#define AIC1106_RESET			    0x40
 
 #define AIC1106_ALMOST_EMPTY	16
 #define AIC1106_ALMOST_FULL		AIC1106_OUTPUT_FIFO_IN_FIFO_DEPTH - 16
-#define AIC1106_SAMPLE_RATE     8000
+#define AIC1106_SAMPLE_RATE   8000
 #define AIC1106_BITS_PER_SAMPLE	16
 
 // ioctl codes
 #define AUDIO_IOCTL_SET_VOL		0
 #define AUDIO_IOCTL_GET_VOL		1
-#define AUDIO_IOCTL_RESET		2
+#define AUDIO_IOCTL_RESET		  2
 #define AUDIO_IOCTL_LOOPBACK	3
 #define AUDIO_IOCTL_IOCANCEL	4
 
-#define AUDIO_FAIL				-1
-#define AUDIO_SUCCESS			0
+#define AUDIO_FAIL				    -1
+#define AUDIO_SUCCESS			    0
 
 
 int		audio_driver_init();
